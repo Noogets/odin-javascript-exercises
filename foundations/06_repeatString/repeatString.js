b47d43e19fbcb3d3161ec28bfdaa3b6d99269ce6
Arno Storms <ast@beerensgroup.com>
2026-03-11 15:36:42 +0100

    Solve exercises 1 - 7

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (word, amount) {
+  if (amount < 0) return "ERROR";
+  let result = "";
+  for (let i = 0; i < amount; i++) {
+    result += word;
+  }
+  return result;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
