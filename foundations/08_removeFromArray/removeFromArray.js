25eabce2cbd5ac34a53882bd0d87d8fab79e9dcf
Noogets <146725759+Noogets@users.noreply.github.com>
2026-03-22 23:12:02 +0100

    Solved exercise 8 and part of 9

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,8 +1,12 @@
-const removeFromArray = function (array) {
-  for (let i = 1; i < arguments.length; i++) {
-    array.splice(array.indexOf(arguments[i]), 1);
-  }
-  return array;
+// const removeFromArray = function (array) {
+//   for (let i = 1; i < arguments.length; i++) {
+//     array.splice(array.indexOf(arguments[i]), 1);
+//   }
+//   return array;
+// };
+
+const removeFromArray = function (array, ...valuesToRemove) {
+  return array.filter((item) => !valuesToRemove.includes(item));
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (int1, int2) {
+  let arr = [];
+  const min = Math.min(int1, int2);
+  const max = Math.max(int1, int2);
+  if (
+    min <= 0 ||
+    max <= 0 ||
+    !Number.isInteger(min) ||
+    !Number.isInteger(max) ||
+    !Number.isNaN(min) ||
+    !Number.isNaN(max)
+  ) {
+    return "ERROR";
+  } else {
+    for (let i = min; i <= max; i++) {
+      arr.push(i);
+    }
+    return arr.reduce((sum, current) => sum + current);
+  }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
