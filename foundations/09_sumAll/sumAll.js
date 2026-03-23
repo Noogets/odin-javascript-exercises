1848cf36fe475140780c37be703ec2372925f351
Arno Storms <ast@beerensgroup.com>
2026-03-23 11:53:52 +0100

    Finished exercise 9 and solved 10 and 11

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,17 +1,17 @@
 const sumAll = function (int1, int2) {
   let arr = [];
-  const min = Math.min(int1, int2);
-  const max = Math.max(int1, int2);
   if (
-    min <= 0 ||
-    max <= 0 ||
-    !Number.isInteger(min) ||
-    !Number.isInteger(max) ||
-    !Number.isNaN(min) ||
-    !Number.isNaN(max)
+    int1 <= 0 ||
+    int2 <= 0 ||
+    !Number.isInteger(int1) ||
+    !Number.isInteger(int2) ||
+    isNaN(int1) ||
+    isNaN(int2)
   ) {
     return "ERROR";
   } else {
+    const min = Math.min(int1, int2);
+    const max = Math.max(int1, int2);
     for (let i = min; i <= max; i++) {
       arr.push(i);
     }
